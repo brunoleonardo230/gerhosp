00b96389620f335ea86949b91bbf4085dfad57eb
Bruno Leonardo <bruno.leonardo23@hotmail.com>
2017-02-13 17:47:40 -0300

    test de Faker

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -18,11 +19,18 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('username',25);
+            $table->string('username',25)->unique();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
+
+        factory(User::class)->create([
+            'name' => 'Adminstrador',
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => md5('admin'),
+            ]);
     }
 
     /**
